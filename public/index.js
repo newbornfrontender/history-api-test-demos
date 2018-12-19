83629678f5557c27c6b2b2343f021a8efc0151ad
Infant Frontender <newbornfrontender@gmail.com>
2018-12-19 11:35:51 +0300

    Добавил заключительный пример

--- public/index.js
+++ public/index.js
@@ -1,14 +1,36 @@
 function show(output) {
-  let p = document.createElement('p');
+  const p = document.createElement('p');
   p.textContent = output;
 
   document.querySelector('#output').appendChild(p);
 
   setTimeout(() => document.querySelector('#output').removeChild(p), 4000);
-};
+}
+
+function loadPerson() {
+  show('loadPerson');
+
+  const hs = JSON.stringify(history.state);
+  const data = JSON.parse(hs);
+  console.log(data.pid);
+  show(data.pid);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#link').addEventListener('click', () => show('clicked'))
+  document.querySelector('#link').addEventListener('click', (event) => {
+    show('clicked');
+
+    event.preventDefault();
+
+    const href = event.currentTarget.href;
+    const personId = event.currentTarget.getAttribute('data-person');
+    show(href);
+
+    // history.pushState({ pid: personId }, 'title', href);
+    history.replaceState({ pid: personId }, 'title', href);
+
+    loadPerson();
+  });
 
   window.addEventListener('hashchange', () => show('hashchange'));
   window.addEventListener('popstate', () => show('popstate'));
