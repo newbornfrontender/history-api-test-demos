91cb49a832139a33d77df85afe0c9e299a781a07
Infant Frontender <newbornfrontender@gmail.com>
2018-12-19 11:01:14 +0300

    Добавил примеры

--- public/index.js
+++ public/index.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', () =>
-  console.log('DOM was loaded!'),
-);
-
-const show = (output) => {
-  const p = document.createElement('p');
+function show(output) {
+  let p = document.createElement('p');
   p.textContent = output;
 
-  document.querySelector('output').appendChild(p);
+  document.querySelector('#output').appendChild(p);
 
-  setTimeout(() => document.querySelector('output').removeChild(p), 4000);
+  setTimeout(() => document.querySelector('#output').removeChild(p), 4000);
 };
+
+function hashChange(event) {
+  show('hashchange')
+}
+
+function popState(event) {
+  show('popstate')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('hashchange', hashChange);
+  window.addEventListener('popstate', popState);
+});
