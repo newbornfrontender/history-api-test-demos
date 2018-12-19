7a64d86f1e0fc50ef10d3718a24f7353a7f4e55c
Infant Frontender <newbornfrontender@gmail.com>
2018-12-19 10:32:36 +0300

    Добавил примеры

--- public/index.js
+++ public/index.js
@@ -1,4 +1,13 @@
 const show = (output) =>
-  (document.querySelector('#output').innerHTML = `<p>${output}</p>`);
+  (document.querySelector('#output').innerHTML += `<p>${output}</p>`);
 
 show(`location.href: ${location.href}`);
+show(`location.search: ${location.search || 'nothing'}`);
+show(`location.hash: ${location.hash || 'nothing'}`);
+
+// history.go(-3);
+// history.back();
+// history.forward()
+
+// location.reload()
+// location.replace('https://www.google.com')
